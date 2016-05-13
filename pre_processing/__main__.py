de23b373c94ce4c0d61532499aab2bd3cd937e46
Rogers Prates <rogersprates@gmail.com>
2016-05-13 18:35:43 -0300

    Extração do word_set

--- pre_processing/__main__.py
+++ pre_processing/__main__.py
@@ -1,1 +1,21 @@
-print "Hellow World"
+import re
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('files/pbr_0216.xml')
+
+root = tree.getroot()
+
+for news in root[0]:
+	try:
+		doc = news.getchildren()
+		doc = doc[2].text + doc[3].text
+		doc = doc.lower().split()
+		doc = re.sub('\W*<', ' ', doc)
+	except:
+		pass
+
+for word in doc:
+	print word
+#parser
+
+#save new file
