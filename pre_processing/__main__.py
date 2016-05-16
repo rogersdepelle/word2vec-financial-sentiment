85b1adee07b9fcb0b6362f341f39e4a55988ba4e
Rogers Prates <rogersprates@gmail.com>
2016-05-16 17:52:29 -0300

    Geração do arquivo test

--- pre_processing/__main__.py
+++ pre_processing/__main__.py
@@ -3,17 +3,27 @@
 import csv
 import json
 import re
+import random
 
 from nltk.corpus import stopwords as nltk_stopwords
 from xml.etree import ElementTree
 from datetime import datetime
 
 
-petr4_daily_file = open('files/petr4_daily.csv', 'rb')
+def gen_test(pbr_file):
+    test_keys = random.sample(pbr_file, int(len(pbr_file)*0.2))
+    test = {}
+    for key in test_keys:
+        test[key] = pbr_file[key]
+        del pbr_file[key]
+    return test
+
+
+petr4_daily_file = open('files/petr4_daily.csv', 'r')
 petr4_daily = csv.DictReader(petr4_daily_file)
 petr4_daily = [x for x in petr4_daily]
 
-petr4_weekly_file = open('files/petr4_weekly.csv', 'rb')
+petr4_weekly_file = open('files/petr4_weekly.csv', 'r')
 petr4_weekly = csv.DictReader(petr4_weekly_file)
 petr4_weekly = [x for x in petr4_weekly]
 
@@ -43,8 +53,9 @@ for news in pbr[0]:
                     else:
                         label = "negative"
 
-                    doc = doc[2].text + doc[3].text
-                    clean_doc = doc.lower().split()
+                    doc = doc[2].text + " " + doc[3].text
+                    clean_doc = doc.lower()
+                    clean_doc = re.sub("[^a-z]", " ", clean_doc).split()
 
                     word_list = set()
                     for word in clean_doc:
@@ -68,9 +79,17 @@ for news in pbr[0]:
 petr4_daily_file.close()
 petr4_weekly_file.close()
 
+test_with_duplicates = open('files/test_with_duplicates.json', 'w')
+json.dump(gen_test(pbr_with_duplicates), test_with_duplicates)
+test_with_duplicates.close()
+
 training_with_duplicates = open('files/training_with_duplicates.json', 'w')
 json.dump(pbr_with_duplicates, training_with_duplicates)
 training_with_duplicates.close()
+
+test_without_duplicates = open('files/test_without_duplicates.json', 'w')
+json.dump(gen_test(pbr_without_duplicates), test_without_duplicates)
+test_without_duplicates.close()
 
 training_without_duplicates = open('files/training_without_duplicates.json', 'w')
 json.dump(pbr_without_duplicates, training_without_duplicates)
